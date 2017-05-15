6644abaa94000d05040c667474f2d5e582a7d4e4
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-05-15 12:12:39 +0300

    added collectionView

---
+++ iTravelSample/iTravelSample/SiSCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SiSCollectionViewCell.swift
+//  iTravelSample
+//
+//  Created by Stanly Shiyanovskiy on 15.05.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class SiSCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imagePersonOutlet: UIImageView!
+    
+}
--- iTravelSample/iTravelSample/SiSNewTripViewController.swift
+++ iTravelSample/iTravelSample/SiSNewTripViewController.swift
@@ -8,72 +8,50 @@
 
 import UIKit
 
-class SiSNewTripViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+class SiSNewTripViewController: UIViewController, UITextFieldDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    let countriesDict = [0 : "Боливия", 1 : "ЮАР", 2 : "Египет", 3 : "Турция", 4 : "Кипр", 5 : "Италия"]
-    let townsDict = [0 : "Арош", 1 : "Ренато", 2 : "Лисаур", 3 : "Уиннтито", 4 : "Левефазис", 5 : "Чипува"]
+    let countriesArray = ["Боливия", "ЮАР", "Египет", "Турция", "Кипр", "Италия"]
+    let townsArray = ["Арош", "Ренато", "Лисаур", "Уиннтито", "Левефазис", "Чипува"]
+    let pickerDataArray = [countriesDict, townsDict]
+    
+    let reuseCollectionViewIdentifier = "person"
+    var items = ["Степан","Лариса Ивановна","Крошка Сын","сосед Жора","друг Жоры"]
+    
 
-    @IBOutlet weak var targetPlace: UITextFieldX!
-    @IBOutlet weak var startTripDate: UITextFieldX!
-    @IBOutlet weak var endTripDate: UITextFieldX!
-    @IBOutlet weak var participantsCount: UILabel!
-    @IBOutlet weak var pickerView: UIPickerView!
-    @IBOutlet weak var blurEffect: UIVisualEffectView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Создание поездки"
-        self.pickerView.alpha = 0
-        self.pickerView.isHidden = true
-        self.blurEffect.alpha = 0
-        self.blurEffect.isHidden = true
 
-    }
-    
-    // MARK: - Picker View -
-    
-    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-        var attributedString : NSAttributedString
-        if component == 0 {
-            attributedString = NSAttributedString(string: countriesDict[row]!, attributes: [NSForegroundColorAttributeName : UIColor.init(colorLiteralRed: 72/255, green: 179/255, blue: 229/255, alpha: 1)])
-        } else {
-            attributedString = NSAttributedString(string: townsDict[row]!, attributes: [NSForegroundColorAttributeName : UIColor.red])
-        }
-        
-        return attributedString
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return countriesDict.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if component == 0 {
-            return countriesDict[row]
-        } else {
-            return townsDict[row]
-        }
     }
     
     // MARK: - Text Field -
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        
-        self.pickerView.isHidden = false
-        self.blurEffect.isHidden = false
-        
-        UIView.animate(withDuration: 0.3) {
-            self.pickerView.center.y = self.view.bounds.midX
-            self.pickerView.alpha = 1.0
-            self.blurEffect.alpha = 1.0
-            
-        }
-        
-        
-        return false
+    // MARK: - UICollectionViewDataSource protocol -
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
     }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseCollectionViewIdentifier, for: indexPath as IndexPath) as! SiSCollectionViewCell
+        
+        // Use the outlet in our cusyom class to get a reference to the UILabel in the cell
+        cell.myLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.white
+        cell.imagePersonOutlet.image = UIImage(named:"person.png")
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol - 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        let title = self.items[indexPath.item]
+        print("You selected cell \(title)!")
+    }
+    
+    
 }
