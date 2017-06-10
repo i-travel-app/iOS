73d6e6db362ad90011008321381845d662c1f52c
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-06-10 19:37:01 +0300

    добавил 2 сущности БД Trip и TargetPlace

---
+++ iTravelSample/iTravelSample/APIStack.swift
@@ -0,0 +1,11 @@
+//
+//  APIStack.swift
+//  iTravelSample
+//
+//  Created by Stanly Shiyanovskiy on 10.06.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import Foundation
+
+// Обращаемся к серверу в первую загрузку для получения idUser и последующей идентификации хозяина программы!!!
