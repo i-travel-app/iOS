512a70f67f8dfc462e9feaeb8dd0cec1cbe7ad92
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-06-13 22:35:29 +0300

    ведется работа с экраном истории поездок

--- iTravelSample/iTravelSample/SiSGeneralViewController.swift
+++ iTravelSample/iTravelSample/SiSGeneralViewController.swift
@@ -7,30 +7,33 @@
 //
 
 import UIKit
+import CoreData
 
-class SiSGeneralViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SiSNewTripDelegate {
+// в данном контроллере обрабатывается логика отображения всех поездок пользователя, в массив вытягиваются из кор даты все поездки и отображаются в таблице в виде: Название поездки, кол-во человек, дата, место; если поездок не было, то отображается приглашение создать таковую
+
+class SiSGeneralViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var cellsArray = [SiSTripModel]()
+    var cellsArray = [Trip]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        for _ in 0..<5 {
-            let tmp = SiSTripModel.init()
-            print(tmp.tripTitle!, tmp.participantsCount!, tmp.targetCountry!, tmp.targetTown!)
-            cellsArray.append(tmp)
-        }
     
         self.tableView.delegate = self
-        self.tableView.dataSource = self        
+        self.tableView.dataSource = self
+        
+        // здесь реализуется логика загрузки историии всех поездок пользователя и добавление их в массив cellsArray
     }
     
-    // MARK: -TableView dataSource-
-    
+    // MARK: - TableView dataSource -
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cellsArray.count + 1
+        // возвращаем количество ячеек равное количеству поездок, если поездок 0, то показываем 1 ячейку с сообщением, что у пользователя не было еще ни одной поездки
+        if cellsArray.isEmpty {
+            return 1
+        } else {
+            return cellsArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -38,26 +41,23 @@ class SiSGeneralViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print(indexPath.row)
-        if indexPath.row == cellsArray.count {
-            let cell: SiSTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "Cell2", for: indexPath as IndexPath) as! SiSTableViewCell
-            return cell
-        } else {
-            let cell: SiSTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath) as! SiSTableViewCell
-            let trip = self.cellsArray[indexPath.row]
-            cell.labelText.text = "\(trip.tripTitle!), участников: \(trip.participantsCount!),\nстрана: \(trip.targetCountry!), город: \(trip.targetTown!)"
-            return cell
+        // здесь реализуем показ списка истории поездок
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        if cellsArray.isEmpty {
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.textAlignment = .center
+            cell.textLabel?.textColor = .red
+            cell.textLabel?.text = "Здесь отображается история Ваших поездок. К сожалению, у Вас не было еще ни одной поездки.\nДобавьте первую, нажав на кнопку ниже."
         }
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.tableView.deselectRow(at: indexPath, animated: true)
-        if indexPath.row == self.cellsArray.count {
+        if indexPath.row == self.cellsArray.count && !self.cellsArray.isEmpty {
             DispatchQueue.main.async(execute: {
-                //self.stopIndicator()
                 let viewController = self.storyboard?.instantiateViewController(withIdentifier: "SiSNewTripViewController") as! SiSNewTripViewController
                 viewController.trip = SiSTripModel()
-                viewController.delegate = self
                 self.navigationController!.pushViewController(viewController, animated: true)
                 return
             })
@@ -65,8 +65,11 @@ class SiSGeneralViewController: UIViewController, UITableViewDelegate, UITableVi
         
     }
     
+    @IBAction func addNewTripAction(_ sender: Any) {
+        
+    }
+    
     func addTripToCellsArray(trip: SiSTripModel) {
-        self.cellsArray.append(trip)
         self.tableView.reloadData()
     }
 
